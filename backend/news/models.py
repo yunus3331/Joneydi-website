4fedb477b9cd5fb5a4a676e42e5b787be426afc8
Davood Azhdari <davood.aj1384@gmail.com>
2026-08-08 20:43:31 +0330

    create db models

--- backend/news/models.py
+++ backend/news/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class News(models.Model):
+    title=models.CharField(max_length=200)
+    content=models.TextField()
+    image=models.ImageField(upload_to='news/')
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    news = models.ForeignKey(
+        News,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="user"
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- backend/pages/models.py
+++ backend/pages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class About(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    updated_at = models.DateTimeField(auto_now=True)
+class ContactInfo(models.Model):
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+    address = models.TextField()
+    updated_at = models.DateTimeField(auto_now=True)
